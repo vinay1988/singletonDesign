92b71406833768997157ac537dc6b3934f8df01a
vinay kumar mishra <vinay.mishra@live.com>
2018-06-26 10:10:05 +0530

    Add project files.

---
+++ Singlaton/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singlaton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singlaton s = Singlaton.Instance;
+            s.Print("Print the message");
+
+            Singlaton s1 = Singlaton.Instance;
+            s1.Print("Print the message again");
+
+            Console.WriteLine("----------------------------------------");
+
+            Singlaton.child s2 = new Singlaton.child();
+            s2.Print("Print from child class");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Singlaton/Singlaton.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singlaton
+{
+    // Make a class sealed otherwise user can create multiple instance
+   public sealed class Singlaton
+    {
+        private static int counter = 0;
+
+        private static Singlaton _instance = null;
+        public static Singlaton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new Singlaton();
+                return _instance;
+            }
+
+        }
+
+      private  Singlaton()
+        {
+            counter++;
+            Console.WriteLine("counter "+counter);
+        }
+        public void Print(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        //
+        //public class child:Singlaton
+        //{
+
+        //}
+    }
+}
